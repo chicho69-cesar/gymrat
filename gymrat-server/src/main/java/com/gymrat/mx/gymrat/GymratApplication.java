871c52a56bc52ae183a691e48b86b7fcca566133
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-02-22 19:58:07 -0600

    Init gymrat server app

---
+++ gymrat-server/src/main/java/com/gymrat/mx/gymrat/GymratApplication.java
@@ -0,0 +1,11 @@
+package com.gymrat.mx.gymrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymratApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GymratApplication.class, args);
+	}
+}
---
+++ gymrat-server/src/test/java/com/gymrat/mx/gymrat/ProyectoItaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymrat.mx.gymrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoItaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

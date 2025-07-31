b66bdd5e7097497e519975386e38bfc72c9f773f
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-31 08:56:33 -0600

    Add exercise repositories

---
+++ domain/repositories/exercise.repository.ts
@@ -0,0 +1,10 @@
+import { ExerciseDto } from 'domain/dtos/exercise.dto'
+import { Exercise } from 'domain/entities/exercise.entity'
+
+export abstract class ExerciseRepository {
+  abstract getExerciseById(id: string): Promise<Exercise | null>
+  abstract getAllExercises(): Promise<Exercise[]>
+  abstract createExercise(exercise: ExerciseDto): Promise<Exercise>
+  abstract updateExercise(id: string, exercise: ExerciseDto): Promise<Exercise>
+  abstract deleteExercise(id: string): Promise<void>
+}
---
+++ infrastructure/repositories/exercise.repository.impl.ts
@@ -0,0 +1,32 @@
+import { ExerciseDataSource } from 'domain/datasources/exercise.datasource'
+import { ExerciseDto } from 'domain/dtos/exercise.dto'
+import { Exercise } from 'domain/entities/exercise.entity'
+import { ExerciseRepository } from 'domain/repositories/exercise.repository'
+
+export class ExerciseRepositoryImpl implements ExerciseRepository {
+  private readonly dataSource: ExerciseDataSource
+
+  constructor(dataSource: ExerciseDataSource) {
+    this.dataSource = dataSource
+  }
+
+  getExerciseById(id: string): Promise<Exercise | null> {
+    return this.dataSource.getExerciseById(id)
+  }
+
+  getAllExercises(): Promise<Exercise[]> {
+    return this.dataSource.getAllExercises()
+  }
+
+  createExercise(exercise: ExerciseDto): Promise<Exercise> {
+    return this.dataSource.createExercise(exercise)
+  }
+
+  updateExercise(id: string, exercise: ExerciseDto): Promise<Exercise> {
+    return this.dataSource.updateExercise(id, exercise)
+  }
+
+  deleteExercise(id: string): Promise<void> {
+    return this.dataSource.deleteExercise(id)
+  }
+}

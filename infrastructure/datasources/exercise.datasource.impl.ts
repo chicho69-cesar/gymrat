07c848a46a9367b505cc8b99f46fb59c7ef98a4f
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-31 08:55:39 -0600

    Add exercise datasource impl

---
+++ infrastructure/datasources/exercise.datasource.impl.ts
@@ -0,0 +1,97 @@
+import { SQLiteDatabase } from 'expo-sqlite'
+
+import { generateId } from 'config/helpers/create-uuid'
+import { ExerciseDataSource } from 'domain/datasources/exercise.datasource'
+import { ExerciseDto } from 'domain/dtos/exercise.dto'
+import { Exercise } from 'domain/entities/exercise.entity'
+import { ExerciseMapper } from 'infrastructure/mappers/exercise.mapper'
+
+export class ExerciseDataSourceImpl implements ExerciseDataSource {
+  private readonly db: SQLiteDatabase
+
+  constructor(db: SQLiteDatabase) {
+    this.db = db
+  }
+
+  async getExerciseById(id: string): Promise<Exercise | null> {
+    try {
+      const exercise = await this.db.getFirstAsync<Exercise>(
+        /* sql */`
+          SELECT * FROM Exercise WHERE id = ?
+        `,
+        [id]
+      )
+
+      return exercise
+    } catch (error) {
+      console.error('Error fetching exercise by ID:', error)
+      return null
+    }
+  }
+
+  async getAllExercises(): Promise<Exercise[]> {
+    try {
+      const exercises = await this.db.getAllAsync<Exercise>(
+        /* sql */`
+          SELECT * FROM Exercise
+        `
+      )
+
+      return exercises
+    } catch (error) {
+      console.error('Error fetching all exercises:', error)
+      return []
+    }
+  }
+
+  async createExercise(exercise: ExerciseDto): Promise<Exercise> {
+    try {
+      const id = generateId()
+
+      const newExercise = await this.db.runAsync(
+        /* sql */`
+          INSERT INTO Exercise (id, name, description, rest)
+          VALUES (?, ?, ?, ?)
+        `,
+        [id, exercise.name, exercise.description || '', exercise.rest]
+      )
+
+      return ExerciseMapper.toEntity(exercise, newExercise.lastInsertRowId.toString())
+    } catch (error) {
+      console.error('Error creating exercise:', error)
+      throw new Error('Failed to create exercise')
+    }
+  }
+
+  async updateExercise(id: string, exercise: ExerciseDto): Promise<Exercise> {
+    try {
+      await this.db.runAsync(
+        /* sql */`
+          UPDATE Exercise
+          SET name = ?, description = ?, rest = ?
+          WHERE id = ?
+        `,
+        [exercise.name, exercise.description || '', exercise.rest, id]
+      )
+
+      return ExerciseMapper.toEntity(exercise, id)
+    } catch (error) {
+      console.error('Error updating exercise:', error)
+      throw new Error('Failed to update exercise')
+    }
+  }
+
+  async deleteExercise(id: string): Promise<void> {
+    try {
+      await this.db.runAsync(
+        /* sql */`
+          DELETE FROM Exercise WHERE id = ?
+        `,
+        [id]
+      )
+    } catch (error) {
+      console.error('Error deleting exercise:', error)
+      throw new Error('Failed to delete exercise')
+    }
+  }
+}
